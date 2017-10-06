044ba30d536c09864f906b20eee351a91344db41
Morgan Fudge <mfudge92@gmail.com>
2017-10-06 15:07:41 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def display_board(cels)
+  puts " #{cels[0]} | #{cels[1]} | #{cels[2]} "
+  puts "-----------"
+  puts " #{cels[3]} | #{cels[4]} | #{cels[5]} "
+  puts "-----------"
+  puts " #{cels[6]} | #{cels[7]} | #{cels[8]} "
+end
